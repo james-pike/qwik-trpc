ef542cc8c00edb34a5b041aa8c3f482b928b7499
James Pike <jamesandrewpike@gmail.com>
2024-11-11 22:18:12 -0500

    m

--- src/entry.vercel-edge.tsx
+++ src/entry.vercel-edge.tsx
@@ -15,8 +15,11 @@ import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
 import render from "./entry.ssr";
 
-declare global {
-  interface QwikCityPlatform extends PlatformVercel {}
-}
+
+  declare global {
+    type QwikCityPlatform = PlatformVercel;
+  }
+  
+
 
 export default createQwikCity({ render, qwikCityPlan, manifest });
